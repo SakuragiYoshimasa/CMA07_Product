9420cc2b214098a06765d8154d4231567558cefc
片山怜 <Rei@Rei-Katayama.local>
2016-02-18 23:53:19 +0900

    mauseYに連動させる途中です。

--- raintest/src/ofApp.cpp
+++ raintest/src/ofApp.cpp
@@ -8,12 +8,15 @@ void ofApp::setup(){
     ofEnableAlphaBlending();
     ofSetFrameRate(30);
     ofSetLineWidth(0.5);
+    mouseX = ofGetWidth()/2;
+    mouseY = ofGetHeight()/2;
     
-    for (int i = 0; i<=NUM; i++) {
-        rainX[i] = ofRandom(5,ofGetWidth()-5);
-        rainY1[i] = ofRandom(-ofGetWidth(),-100);
-        rainY2[i] = rainY1[i] + ofRandom(10,100);
-        speed_y[i] = ofRandom(10, 30);
+    for (int i = 0; i<NUM; i++) {
+        rainX[i] = ofRandom(5.0,ofGetWidth()-5.0);
+        rainY1[i] = ofRandom(-ofGetWidth(),-100.0);
+        rainY2[i] = rainY1[i] + ofRandom(10.0, 100.0);
+        speed_y[i] = 10;//= ofRandom(10.0, 30.0);
+        
     }
     
 }
@@ -21,35 +24,46 @@ void ofApp::setup(){
 //--------------------------------------------------------------
 void ofApp::update(){
     
-    for (int i = 0; i<=NUM; i++) {
+    
+    
+    for (int i = 0; i<NUM; i++) {
         
         rainY1[i] = rainY1[i] + speed_y[i];
         rainY2[i] = rainY2[i] + speed_y[i];
         
+        // triangleY[i] = (mouseY / mouseX) * rainX[i];
+        
+        
+        
         if (rainY1[i] > ofGetHeight()) {
-            rainY1[i] = -100;
-            rainY2[i] = rainY1[i] + ofRandom(10,100);
+            rainY1[i] = -100.0;
+            rainY2[i] = rainY1[i] + ofRandom(10.0, 100.0);
         }
         
-        if (rainX[i] > 100 && rainX[i] <500) {
-            if (rainY2[i] >ofGetHeight()/2-15 && rainY2[1]<ofGetHeight()/2+15) {
-                rainY1[i] = -1000;
-                rainY2[i] = rainY1[i] + ofRandom(10,100);
+        if (rainX[i] > ofGetWidth()/2 && rainX[i] < ofGetWidth()/2 +300) {
+            if ((rainY2[i] = rainX[i] * mouseY / mouseX)){
+                
+                rainY1[i] = -100.0;
+                rainY2[i] = rainY1[i] + ofRandom(10.0, 100.0);
             }
         }
     }
 }
 
-
 //--------------------------------------------------------------
 void ofApp::draw(){
     
     
-    for (int i = 0; i<=NUM; i++) {
+    for (int i = 0; i<NUM; i++) {
         ofLine(rainX[i], rainY1[i], rainX[i], rainY2[i]);
     }
     
-    ofLine(100, ofGetHeight()/2, 500, ofGetHeight()/2);
+    ofTranslate(ofGetWidth()/2,ofGetHeight()/2);
+    
+    triangleX =  sqrt(300*300-mouseY*mouseY);
+    ofLine(0,0, triangleX, mouseY);
+    
+    
     
 }
 
