26009781e836debf4ae33f7bbadb315e681e5ea7
Ryosuke ONOSE <mail@onose.ws>
2016-02-26 00:35:04 +0900

    MidiControl クラスを追加

---
+++ onose_faceControl/src/MidiControl.cpp
@@ -0,0 +1,9 @@
+//
+//  MidiControl.cpp
+//  onose_faceControl
+//
+//  Created by Ryosuke ONOSE on 2/26/16.
+//
+//
+
+#include "MidiControl.hpp"
---
+++ onose_faceControl/src/MidiControl.h
@@ -0,0 +1,14 @@
+//
+//  MidiControl.hpp
+//  onose_faceControl
+//
+//  Created by Ryosuke ONOSE on 2/26/16.
+//
+//
+
+#ifndef MidiControl_hpp
+#define MidiControl_hpp
+
+#include <stdio.h>
+
+#endif /* MidiControl_hpp */
